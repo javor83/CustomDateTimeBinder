0dbb53df871dfe4f4dcc38972ece9bc332cdc615
Явор Налбантов <javor.nalbantov@gmail.com>
2026-02-20 21:29:33 +0200

    как разбираме към точно поле да се хване, а не към целия обект ?

--- demo_proj/Models/DateAgeModelBinderProvider.cs
+++ demo_proj/Models/DateAgeModelBinderProvider.cs
@@ -23,7 +23,8 @@ namespace demo_proj.Models
                 ArgumentException.ThrowIfNullOrEmpty(nameof(context));
             }
             
-            var x = context.Metadata.ModelType;
+            var x = context.Metadata.ModelType;//типът е Post object типа-
+            //изпращаме Person, следователно context.Metadata.ModelType = Person type
             if (x == typeof(DateTime) || x == typeof(DateTime?))
             {
                 return new DateAgeTimeModelBinder(this._dateFormat);
