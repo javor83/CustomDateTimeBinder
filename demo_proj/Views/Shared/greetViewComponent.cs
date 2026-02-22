936b3725db25ead2a4da1c94dfcb39207d84ffd8
Явор Налбантов <javor.nalbantov@gmail.com>
2026-02-22 09:48:55 +0200

    ok

---
+++ demo_proj/Views/Shared/greetViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace demo_proj.Views.Shared
+{
+    public class greetViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string name = "javor")
+        {
+            return View("Default", name);
+        }
+
+    }
+}
---
+++ demo_proj/Views/Shared/pageViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace demo_proj.Views.Shared
+{
+    public class pageViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke(string name = "javor")
+        {
+            return View("Default", name);
+        }
+    }
+}
